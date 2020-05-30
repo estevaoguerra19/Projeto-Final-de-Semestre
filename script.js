aad1dee4c15fd205afe9f572413720bb166c74f6
Estêvão Guerra <estevao2001@hotmail.com>
2020-05-30 19:03:36 -0300

    Projeto de aplicação de HTML, CSS e JS

---
+++ script.js
@@ -0,0 +1,16 @@
+function calcular() {
+    var txtv = window.document.querySelector('input#txtvel');
+    var res = window.document.querySelector('div#res');
+    var vel = Number(txtv.value);
+    res.innerHTML = `<p>Sua velocidade atual é de <strong>${vel}Km/h</strong></p>`
+    if (vel >= 80) {
+       res.innerHTML += `<p>Você está <strong>MULTADO!</strong> por excesso de velocidade.</p>`
+    }else if (vel < 80 && vel >= 20) {
+        res.innerHTML += `<p>Você está dentro das regras de trânsito. Cuidado ao dirigir a pista.</p>`
+        }else if (vel > 0 && vel < 20) {
+            res.innerHTML += `<p>Por favor, acelere a velocidade, você pode causar um acidente.</p>`
+            }else {
+                res.innerHTML += `<p>Insira um valor válido.</p>`
+                }
+                res.innerHTML += `<p>Dirija sempre com cinto de segurança e <strong>BEBA COM MODERAÇÃO!</strong></p>`
+}
